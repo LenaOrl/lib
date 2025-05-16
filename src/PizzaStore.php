b90f201d0c40ebce584ee0e84de1e557b029992b
LenaOrl <orlovaleksandr932@gmail.com>
2025-05-17 00:02:11 +0300

    Update PizzaStore.php

--- src/PizzaStore.php
+++ src/PizzaStore.php
@@ -4,18 +4,7 @@ namespace Lena\Zad6;
 
 class PizzaStore
 {
-    public function createPizza(string $type) 
-    {
-      switch ($type) {
-          case 'pepperoni':
-              return new PepperoniPizza();
-              case 'chesse':
-                return new CheesePizza();
-              case 'veggie':
-                  return new VeggiePizza();
-              return null;
-              }
-          }
+    public function createPizza(string $type): void;
     public function orderPizza(string $type): void   
   {
     $pizza=$this->createPizza($type);
